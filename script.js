6bd97f2674af5f0a42a55ee1c57c516a41f87e3f
Mario Müller <mariomuller@macbook-pro.speedport.ip>
2023-08-25 22:59:40 +0200

    js render main content + emptyCard // new JASON

---
+++ script.js
@@ -0,0 +1,15 @@
+function render() {
+
+    let menuContent = document.getElementById('menuContent');
+    menuContent.innerHTML = '';
+
+    for (let i = 0; i < food.length; i++) {
+
+        menuContent.innerHTML += loadMenuItem(i);
+    }
+    loadEmptyCard();
+    // closeCart();
+    // loadModal();
+    // hideModal();
+    // templateSumMobile();
+}
