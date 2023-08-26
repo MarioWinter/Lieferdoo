060e1bfd123efbf116f45cdfdfa3066c9be6c968
Mario Müller <mariomuller@macbook-pro.speedport.ip>
2023-08-26 11:52:48 +0200

    js addToCard

--- script.js
+++ script.js
@@ -13,3 +13,39 @@ function render() {
     // hideModal();
     // templateSumMobile();
 }
+
+function addToCard(index, item) {
+    let shoppingBasket = document.getElementById('shoppingCardContent');
+    shoppingBasket.innerHTML = '';
+
+    shoppingBasket.innerHTML = countUp(index);
+    let price = countPriceUp(index);
+    
+    loadBasket(index, price);
+
+
+}
+
+
+function countUp(i) {
+        let menu = food[i];
+        let menuAmount = menu['amount'];
+
+        if (menuAmount == 0) {
+            menuAmount = menuAmount++
+        }        
+}
+
+
+function countPriceUp(i) {
+    let menu = food[i];
+    let menuAmount = menu['amount'];
+    let menuPrice = menu['price'];
+
+    if (menuAmount <= 1) {
+        menuPrice = menuPrice * 2
+    }
+
+    return menuPrice
+
+}
